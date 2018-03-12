65f0231189ea260202835c7a1d3e9212fe822b0c
Bderiel <bderiel@gmail.com>
2018-03-11 21:45:58 -0400

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,19 @@ module.exports = {
       name: 'src',
       path: `${__dirname}/src/`,
     },
-  }, 'gatsby-plugin-react-next', 'gatsby-transformer-remark', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
-  // pathPrefix: '/bderiel.com',
+  }, 'gatsby-plugin-react-next', 'gatsby-transformer-remark', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sass',
+  {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      trackingId: 'UA-115539133-1',
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+    },
+  }],
 };
 
+// const t =
--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
 const quotes = ['I AM A SOFTWARE ENGINEER', 'I AM A PROBLEM SOLVER', 'I AM A TEAM PLAYER', 'I AM AVAILABLE FOR HIRE', 'I AM JSMASTER', 'ARE YOU STILL HERE?', 'I THINK KANYE IS A GENIUS', 'HOW MANY OF THESE DID HE WRITE?'];
+import mugshot from '../../../img/mugshot.jpg';
 
 class Banner extends Component {
   constructor() {
@@ -27,21 +28,22 @@ class Banner extends Component {
 
   render() {
     const { quoteIdx } = this.state;
-    return (<section id="banner" className="major">
-      <div className="fade inner">
-        <header className="major">
-          <h1>Brian Deriel</h1>
-        </header>
-        <div className="content">
-          <div onAnimationIteration={this.handleAnimation} className="typewriter">
-            <p id="bio">{quotes[quoteIdx % quotes.length]}</p>
+    return (
+      <section id="banner" className="major">
+        <div className="fade inner">
+          <header className="major">
+            <h1>Brian Deriel</h1>
+          </header>
+          <div className="content">
+            <div onAnimationIteration={this.handleAnimation} className="typewriter">
+              <p id="bio">{quotes[quoteIdx % quotes.length]}</p>
+            </div>
+            <ul className="actions">
+              <li onClick={this.handleClick}><span className="button next scrolly">Projects</span></li>
+            </ul>
           </div>
-          <ul className="actions">
-            <li onClick={this.handleClick}><span className="button next scrolly">Projects</span></li>
-          </ul>
         </div>
-      </div>
-            </section>
+      </section>
     );
   }
 }
